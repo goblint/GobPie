3984b14550ce8b9d515737c57dfa51a31526c1de
Juhan Oskar Hennoste <juhan.hennoste@ut.ee>
2023-04-29 15:20:13 +0300

    Add adb examples

---
+++ adb_examples/thesis_example.c
@@ -0,0 +1,36 @@
+#include "precise_stdlib.h"
+#include <stdio.h>
+#include <pthread.h>
+
+pthread_mutex_t lukk = PTHREAD_MUTEX_INITIALIZER;
+
+void f(int a) {
+    printf("%i", a);
+}
+
+int g(int b) {
+    if (b >= 50) {
+        f(b - 50);
+    } else {
+        f(b);
+        f(b + 12);
+    }
+}
+
+int h(int c) {
+    if (c == 0) {
+        pthread_mutex_lock(&lukk);
+    }
+    if (c > 0) {
+        printf("%i", c);
+    }
+    if (c == 0) {
+        pthread_mutex_unlock(&lukk);
+    }
+}
+
+int main() {
+    int i = rand() % 100;
+    g(i);
+    h(i);
+}
