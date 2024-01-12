e2a99c333f6e5cc215ca2d005460b7848fec3fbd
Karoliine Holter <karoliine.holter@hotmail.com>
2024-01-12 10:57:42 +0200

    Add equals and hash to AnalyzeParams

--- src/main/java/api/messages/params/AnalyzeParams.java
+++ src/main/java/api/messages/params/AnalyzeParams.java
@@ -1,5 +1,7 @@
 package api.messages.params;
 
+import java.util.Objects;
+
 public class AnalyzeParams {
 
     boolean reset;
@@ -8,4 +10,16 @@ public class AnalyzeParams {
         this.reset = reset;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AnalyzeParams that = (AnalyzeParams) o;
+        return reset == that.reset;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reset);
+    }
 }
