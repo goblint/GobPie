94417f37c1d287ff8eb041bf342bb2e5071abc57
Karoliine Holter <44437975+karoliineh@users.noreply.github.com>
2024-02-12 10:55:31 +0200

    Check for logging messages and fix `preAnalyseTest` (#1)
    * Remove duplicated junit dependencies from pom
    * Fix surefire plugin error by adding junit dependency
    * Remove unused imports
    * Add ability to check for logging messages
    * Format code
    * Fix preAnalyseTest

--- src/test/java/GoblintAnalysisTest.java
+++ src/test/java/GoblintAnalysisTest.java
@@ -9,41 +9,29 @@ import goblintserver.GoblintServer;
 import gobpie.GobPieConfiguration;
 import magpiebridge.core.AnalysisConsumer;
 import magpiebridge.core.MagpieServer;
-import nl.altindag.log.LogCaptor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.LogEvent;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
+import uk.org.webcompere.systemstubs.stream.SystemOut;
 
-import javax.swing.*;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(SystemStubsExtension.class)
 class GoblintAnalysisTest {
 
-    private final List<String> logMessages = new ArrayList<>();
-
-
-
-    public void append(LogEvent event) {
-        logMessages.add(event.getMessage().getFormattedMessage());
-    }
-
-    public boolean contains(String logMessage) {
-        return logMessages.contains(logMessage);
-    }
+    @SystemStub
+    private SystemOut systemOut;
 
     /**
      * Mock test to ensure @analyze function
      * behaviour in abort situation
-     *
      */
     @Test
     void abortAnalysis() throws IOException {
@@ -54,11 +42,6 @@ class GoblintAnalysisTest {
         GoblintService goblintService = mock(GoblintService.class);
         GobPieConfiguration gobPieConfiguration = mock(GobPieConfiguration.class);
         GoblintConfWatcher goblintConfWatcher = mock(GoblintConfWatcher.class);
-        org.apache.logging.log4j.Logger log = LogManager.getLogger(GoblintAnalysis.class);
-
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        String consoleOutput = byteArrayOutputStream.toString();
 
         GoblintAnalysis goblintAnalysis = new GoblintAnalysis(magpieServer, goblintServer, goblintService, gobPieConfiguration, goblintConfWatcher);
 
@@ -66,7 +49,7 @@ class GoblintAnalysisTest {
         when(goblintServer.isAlive()).thenReturn(true);
         when(goblintConfWatcher.refreshGoblintConfig()).thenReturn(true);
 
-        // Mock that the analyses of goblint have started but not completed (still run)
+        // Mock that the analyses of Goblint have started but not completed (still run)
         when(goblintService.analyze(new AnalyzeParams(false))).thenReturn(new CompletableFuture<>());
 
         // Mock that the incremental analysis is turned off (TODO: not sure why this is checked in reanalyze?)
@@ -79,67 +62,57 @@ class GoblintAnalysisTest {
         goblintAnalysis.analyze(files, analysisConsumer, true);
         goblintAnalysis.analyze(files, analysisConsumer, true);
 
-        System.out.println(consoleOutput);
-
-        System.out.println("--------------- This analysis has been aborted -------------");
-        System.out.println(log.getName());
-
-        //assertTrue(consoleOutput, "--------------- This analysis has been aborted -------------");
-
         // Verify that abortAnalysis was indeed called once
         verify(goblintServer).abortAnalysis();
+        assertTrue(systemOut.getLines().anyMatch(line -> line.contains("--------------- This analysis has been aborted -------------")));
     }
+
     /**
      * Mock test to ensure @preAnalyse function
      * is functional and is called out in @analyze function
-     *
      */
 
 
     @Test
-    void preAnalysetest() {
+    void preAnalyseTest() {
         // Mock everything needed for creating preAnalysis
         MagpieServer magpieServer = mock(MagpieServer.class);
-        GoblintServer goblintServer = mock(GoblintServer.class);
         GoblintService goblintService = mock(GoblintService.class);
-        GobPieConfiguration gobPieConfigurationMock = mock(GobPieConfiguration.class);
+        GobPieConfiguration gobPieConfiguration = mock(GobPieConfiguration.class);
+        GoblintServer goblintServer = spy(new GoblintServer(magpieServer, gobPieConfiguration));
         GoblintConfWatcher goblintConfWatcher = mock(GoblintConfWatcher.class);
 
-
-        GoblintAnalysis goblintAnalysis = new GoblintAnalysis(magpieServer, goblintServer, goblintService, gobPieConfigurationMock, goblintConfWatcher);
+        GoblintAnalysis goblintAnalysis = new GoblintAnalysis(magpieServer, goblintServer, goblintService, gobPieConfiguration, goblintConfWatcher);
 
         // Mock that GoblintServer is alive and everything is fine with Goblint's configuration file
-        when(goblintServer.isAlive()).thenReturn(true);
+        doReturn(true).when(goblintServer).isAlive();
         when(goblintConfWatcher.refreshGoblintConfig()).thenReturn(true);
 
         // Mock that the command to execute is not empty
-        String[] preAnalyzeCommand  = new String[]{"cmake", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON", "-B", "build"};
-        when( gobPieConfigurationMock.getPreAnalyzeCommand()).thenReturn(new String[]{"cmake", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON", "-B", "build"});
+        String[] preAnalyzeCommand = new String[]{"cmake", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON", "-B", "build"};
+        when(gobPieConfiguration.getPreAnalyzeCommand()).thenReturn(preAnalyzeCommand);
 
-        // Mock that the analyses of goblint have started but not completed (still run)
+        // Mock that the analyses of Goblint have started but not completed (still run)
         when(goblintService.analyze(new AnalyzeParams(false))).thenReturn(new CompletableFuture<>());
 
         // Mock that the incremental analysis is turned off (TODO: not sure why this is checked in reanalyze?)
-        when(gobPieConfigurationMock.useIncrementalAnalysis()).thenReturn(true);
+        when(gobPieConfiguration.useIncrementalAnalysis()).thenReturn(true);
 
         // Mock the arguments for calling the goblintAnalyze.analyze method
         Collection<? extends Module> files = new ArrayDeque<>();
         AnalysisConsumer analysisConsumer = mock(AnalysisConsumer.class);
         goblintAnalysis.analyze(files, analysisConsumer, true);
 
-
         // Verify that preAnalysis was indeed called once
         verify(goblintServer).preAnalyse();
+        assertTrue(systemOut.getLines().anyMatch(line -> line.contains("Preanalyze command ran: ")));
     }
 
     /**
-     ** Mock test to ensure @preAnalyse function
-     *
-     *
+     * * Mock test to ensure @preAnalyse function
+     * <p>
+     * <p>
      * is functional and is called out in @analyze function
-     *
-     *
-     *
      */
     @Test
     void preanalyzeError() {
@@ -161,8 +134,8 @@ class GoblintAnalysisTest {
 
         // Mock that the command to execute is not empty
         // Make mistake to catch exception -  correct - {"cmake", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON", "-B", "build"}
-        String[] preAnalyzeCommand  = new String[]{"cmake", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON", "-V", "bdghdhgfjhy"};
-        when(gobPieConfiguration.getPreAnalyzeCommand()).thenReturn(preAnalyzeCommand );
+        String[] preAnalyzeCommand = new String[]{"cmake", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON", "-V", "bdghdhgfjhy"};
+        when(gobPieConfiguration.getPreAnalyzeCommand()).thenReturn(preAnalyzeCommand);
 
         // Mock that the analyses of goblint have started but not completed (still run)
         when(goblintService.analyze(new AnalyzeParams(false))).thenReturn(new CompletableFuture<>());
@@ -179,25 +152,13 @@ class GoblintAnalysisTest {
 
         System.out.println(logger.getLoggerContext().toString());
 
-        assert(logger.getLoggerContext().toString().contains("Running preanalysis command failed. "));
+        assert (logger.getLoggerContext().toString().contains("Running preanalysis command failed. "));
 
         //verify(log, times (1)).info("Running preanalysis command failed. ");
 
         // Verify that preAnalysis was indeed called once
         verify(goblintServer).preAnalyse();
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
