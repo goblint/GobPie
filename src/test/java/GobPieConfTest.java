fe9beeba9fa17eef1d8e8f6ad132d7602c68dfb9
anette <taivereanette@gmail.com>
2024-02-19 13:54:11 +0200

    added file, extra field test

--- src/test/java/GobPieConfTest.java
+++ src/test/java/GobPieConfTest.java
@@ -1,23 +1,19 @@
-import api.messages.params.AnalyzeParams;
 import gobpie.GobPieConfReader;
 import gobpie.GobPieConfiguration;
-import gobpie.GobPieException;
-import gobpie.GobPieExceptionType;
-import magpiebridge.core.MagpieClient;
 import magpiebridge.core.MagpieServer;
-import org.eclipse.lsp4j.MessageParams;
-import org.eclipse.lsp4j.MessageType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.stubbing.Answer;
 import uk.org.webcompere.systemstubs.jupiter.SystemStub;
 import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 import uk.org.webcompere.systemstubs.stream.SystemOut;
 import util.FileWatcher;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.concurrent.CompletableFuture;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -29,13 +25,16 @@ class GobPieConfTest {
     @SystemStub
     private SystemOut systemOut;
 
+    @TempDir
+    static Path tempDir;
+    static Path tempFile;
+
 
     @Test
     void testReadGobPieConfiguration() {
         // Mock everything needed for creating GobPieConfReader
         MagpieServer magpieServer = mock(MagpieServer.class);
         String gobPieConfFileName = GobPieConfTest.class.getResource("gobpieTest1.json").getFile();
-
         GobPieConfReader gobPieConfReader = new GobPieConfReader(magpieServer, gobPieConfFileName);
 
         GobPieConfiguration expectedGobPieConfiguration =
@@ -62,7 +61,6 @@ class GobPieConfTest {
         // Mock everything needed for creating GobPieConfReader
         MagpieServer magpieServer = mock(MagpieServer.class);
         String gobPieConfFileName = GobPieConfTest.class.getResource("gobpieTest2.json").getFile();
-
         GobPieConfReader gobPieConfReader = new GobPieConfReader(magpieServer, gobPieConfFileName);
 
         GobPieConfiguration expectedGobPieConfiguration =
@@ -84,24 +82,75 @@ class GobPieConfTest {
     }
 
     @Test
-    void testGobPieConfigurationFailMissingInRoot() throws InterruptedException {
+    void testGobPieConfigurationFailMissingInRoot() throws InterruptedException, IOException {
         // Mock everything needed for creating GobPieConfReader
         MagpieServer magpieServer = mock(MagpieServer.class);
-        String gobPieConfFileName = "gobpie.json";//GobPieConfTest.class.getResource("gobpie.json").getFile();
-        FileWatcher gobPieConfWatcher = new FileWatcher(Path.of(gobPieConfFileName));
+        FileWatcher gobPieConfWatcher = mock(FileWatcher.class);
+        String gobPieConfFileName =  GobPieConfTest.class.getResource("gobpie.json").getFile();
+
+        //tempFile = Files.createFile(tempDir.resolve("gobpie.json"));
+        //String gobPieConfFileName = String.valueOf(tempFile);
 
         GobPieConfReader gobPieConfReader = new GobPieConfReader(magpieServer, gobPieConfFileName);
-        //MagpieClient client = mock(MagpieClient.class);
-        //MagpieClient client = spy(MagpieClient.class);
-        //doThrow(new InterruptedException()).when(gobPieConfWatcher.waitForModified());
-        GobPieConfiguration actualGobPieConfiguration = gobPieConfReader.readGobPieConfiguration();
-        //delete file
+        //when(Files.exists(any(Path.class))).thenReturn(false);
+        doNothing().when(gobPieConfWatcher).waitForModified();
 
+
+        GobPieConfiguration actualGobPieConfiguration = gobPieConfReader.readGobPieConfiguration();
 
         assertTrue(systemOut.getLines().anyMatch(line -> line.contains("Reading GobPie configuration from json")));
         assertTrue(systemOut.getLines().anyMatch(line -> line.contains("GobPie configuration file is not found in the project root.")));
+    }
 
-        //verify(magpieServer).forwardMessageToClient(new MessageParams(MessageType.Error, "GobPie configuration file is not found in the project root." ));
+
+    @Test
+    void testReadGobPieConfigurationWithExtraField() {
+        // Mock everything needed for creating GobPieConfReader
+        MagpieServer magpieServer = mock(MagpieServer.class);
+        String gobPieConfFileName = GobPieConfTest.class.getResource("gobpieTest4.json").getFile();
+        GobPieConfReader gobPieConfReader = new GobPieConfReader(magpieServer, gobPieConfFileName);
+
+        GobPieConfiguration expectedGobPieConfiguration =
+                new GobPieConfiguration.Builder()
+                        .setGoblintConf("goblint.json")
+                        .setGoblintExecutable("/home/user/goblint/analyzer/goblint")
+                        .setPreAnalyzeCommand(new String[]{"cmake", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON", "-B", "build"})
+                        .setAbstractDebugging(false)
+                        .setShowCfg(false)
+                        .setIncrementalAnalysis(true)
+                        .setExplodeGroupWarnings(false)
+                        .createGobPieConfiguration();
+
+        GobPieConfiguration actualGobPieConfiguration = gobPieConfReader.readGobPieConfiguration();
+
+        assertTrue(systemOut.getLines().anyMatch(line -> line.contains("Reading GobPie configuration from json")));
+        assertTrue(systemOut.getLines().anyMatch(line -> line.contains("GobPie configuration read from json")));
+        assertEquals(expectedGobPieConfiguration, actualGobPieConfiguration);
+    }
+
+    @Test
+    void testReadGobPieConfigurationWithWrongJSONSyntax() {
+        // Mock everything needed for creating GobPieConfReader
+        MagpieServer magpieServer = mock(MagpieServer.class);
+        String gobPieConfFileName = GobPieConfTest.class.getResource("gobpieTest5.json").getFile();
+        GobPieConfReader gobPieConfReader = new GobPieConfReader(magpieServer, gobPieConfFileName);
+
+        GobPieConfiguration expectedGobPieConfiguration =
+                new GobPieConfiguration.Builder()
+                        .setGoblintConf("goblint.json")
+                        .setGoblintExecutable("/home/user/goblint/analyzer/goblint")
+                        .setPreAnalyzeCommand(new String[]{"cmake", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON", "-B", "build"})
+                        .setAbstractDebugging(false)
+                        .setShowCfg(false)
+                        .setIncrementalAnalysis(true)
+                        .setExplodeGroupWarnings(false)
+                        .createGobPieConfiguration();
+
+        GobPieConfiguration actualGobPieConfiguration = gobPieConfReader.readGobPieConfiguration();
+
+        assertTrue(systemOut.getLines().anyMatch(line -> line.contains("Reading GobPie configuration from json")));
+        assertTrue(systemOut.getLines().anyMatch(line -> line.contains("GobPie configuration read from json")));
+        assertEquals(expectedGobPieConfiguration, actualGobPieConfiguration);
     }
 
 }
