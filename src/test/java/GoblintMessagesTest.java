91c08c383526a172f1fa4d72d20f9876f28f69ce
anette <taivereanette@gmail.com>
2024-03-11 12:12:49 +0200

    Tried convert function from json

--- src/test/java/GoblintMessagesTest.java
+++ src/test/java/GoblintMessagesTest.java
@@ -1,8 +1,10 @@
 import analysis.GoblintAnalysis;
+import analysis.GoblintCFGAnalysisResult;
 import analysis.GoblintMessagesAnalysisResult;
 import api.GoblintService;
 import api.json.GoblintMessageJsonHandler;
 import api.messages.GoblintAnalysisResult;
+import api.messages.GoblintFunctionsResult;
 import api.messages.GoblintMessagesResult;
 import api.messages.GoblintPosition;
 import api.messages.params.AnalyzeParams;
@@ -86,6 +88,13 @@ public class GoblintMessagesTest {
         return gson.fromJson(messages, new TypeToken<List<GoblintMessagesResult>>() {}.getType());
     }
 
+    private List<GoblintFunctionsResult> readGoblintResponseJsonFunc() throws IOException {
+        String functions = Files.readString(
+                Path.of(GoblintMessagesTest.class.getResource("messagesResponse.json").getPath())
+        );
+        return gson.fromJson(functions, new TypeToken<List<GoblintFunctionsResult>>() {}.getType());
+    }
+
 
     /**
      * Mock test to ensure that the Goblint warnings received from a response in JSON format
@@ -161,6 +170,62 @@ public class GoblintMessagesTest {
 
         verify(analysisConsumer).consume(response, "GobPie");
     }
+
+
+
+
+    @Test
+    public void testConvertFunctionsFromJson() throws IOException {
+        List<GoblintFunctionsResult> goblintFunctionsResults = readGoblintResponseJsonFunc();
+
+        when(goblintService.functions()).thenReturn(CompletableFuture.completedFuture(goblintFunctionsResults));
+        when(gobPieConfiguration.showCfg()).thenReturn(false);
+
+        goblintAnalysis.analyze(files, analysisConsumer, true);
+
+        URL emptyUrl = new File("").toURI().toURL();
+        GoblintPosition defaultPos = new GoblintPosition(1, 1, 1, 1, emptyUrl);
+        URL exampleUrl = new File("src/example.c").toURI().toURL();
+
+        List<AnalysisResult> response = new ArrayList<>();
+        response.add(
+                new GoblintCFGAnalysisResult(
+                        defaultPos,
+                        "2.0",
+                        "t_fun"
+
+
+                )
+        );
+        response.add(
+                new GoblintMessagesAnalysisResult(
+                        new GoblintPosition(4, 4, 4, 12, exampleUrl),
+                        "[Race] Memory location myglobal (race with conf. 110)",
+                        "Warning",
+                        List.of(
+                                Pair.make(
+                                        new GoblintPosition(10, 10, 2, 21, exampleUrl),
+                                        "write with [mhp:{tid=[main, t_fun@src/example.c:17:3-17:40#top]}, lock:{mutex1}, thread:[main, t_fun@src/example.c:17:3-17:40#top]] (conf. 110)  (exp: & myglobal)"
+                                )
+                        )
+                )
+        );
+        response.add(
+                new GoblintMessagesAnalysisResult(
+                        defaultPos,
+                        "[Race] Memory locations race summary",
+                        "Info",
+                        List.of(
+                                Pair.make(defaultPos, "safe: 0"),
+                                Pair.make(defaultPos, "vulnerable: 0"),
+                                Pair.make(defaultPos, "unsafe: 1"),
+                                Pair.make(defaultPos, "total memory locations: 1")
+                        )
+                )
+        );
+        verify(analysisConsumer).consume(response, "GobPie");
+    }
+
 
 
 }
