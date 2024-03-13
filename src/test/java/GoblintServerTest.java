43eaf86c1531fce1f0591c4bb6de75d23136c919
anette <taivereanette@gmail.com>
2024-03-13 17:18:36 +0200

    pushed changes that were made in 12.03 meeting

--- src/test/java/GoblintServerTest.java
+++ src/test/java/GoblintServerTest.java
@@ -4,12 +4,12 @@ import gobpie.GobPieConfiguration;
 import gobpie.GobPieException;
 import magpiebridge.core.MagpieServer;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 import uk.org.webcompere.systemstubs.jupiter.SystemStub;
 import uk.org.webcompere.systemstubs.stream.SystemOut;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class GoblintServerTest {
 
@@ -20,40 +20,50 @@ public class GoblintServerTest {
     @Test
     public void testStartGoblintServer() {
         /**
-         GoblintServer goblintServer = mock(GoblintServer.class);
-         MagpieServer magpieServer =mock(MagpieServer.class);
-         //GobPieConfiguration gobPieConfiguration = mock(GobPieConfiguration.class);
 
-         String fileName = "gobpieTest7.json";
-         String gobPieConfFileName = GobPieConfTest.class.getResource(fileName).getFile();
-         GobPieConfReader gobPieConfReader = new GobPieConfReader(magpieServer, gobPieConfFileName);
+        GoblintServer goblintServer = mock(GoblintServer.class);
+        MagpieServer magpieServer = mock(MagpieServer.class);
+        //GobPieConfiguration gobPieConfiguration = mock(GobPieConfiguration.class);
 
-         GobPieConfiguration gobPieConfiguration = gobPieConfReader.readGobPieConfiguration();
+        String fileName = "gobpieTest7.json";
+        String gobPieConfFileName = GobPieConfTest.class.getResource(fileName).getFile();
+        GobPieConfReader gobPieConfReader = new GobPieConfReader(magpieServer, gobPieConfFileName);
 
+        GobPieConfiguration gobPieConfiguration = gobPieConfReader.readGobPieConfiguration();
+        Logger log = mock(Logger.class);
+        //doNothing().when(log).isDebugEnabled();
+        //doAnswer().when(log).isDebugEnabled();
+        doReturn(false).when(log).isDebugEnabled();
+        //when(log.isDebugEnabled()).thenReturn(false);
 
-         Main.startGoblintServer(magpieServer, gobPieConfiguration);
+        Main.startGoblintServer(magpieServer, gobPieConfiguration);
 
-         assertTrue(systemOut.getLines().anyMatch(line -> line.contains("Goblint run with command: ")));
-         **/
+        assertTrue(systemOut.getLines().anyMatch(line -> line.contains("Goblint run with command: ")));
+
+        **/
     }
 
 
     @Test
     public void testCheckGoblintVersion() {
         /**
+
         MagpieServer magpieServer = mock(MagpieServer.class);
         String fileName = "gobpieTest7.json";
         String gobPieConfFileName = GobPieConfTest.class.getResource(fileName).getFile();
         GobPieConfReader gobPieConfReader = new GobPieConfReader(magpieServer, gobPieConfFileName);
         GobPieConfiguration gobPieConfiguration = gobPieConfReader.readGobPieConfiguration();
         GoblintServer goblintServer = new GoblintServer(magpieServer, gobPieConfiguration);
+        Logger log = mock(Logger.class);
 
-        goblintServer.checkGoblintVersion();
+        when(log.isDebugEnabled()).thenReturn(false);
+
+        Main.startGoblintServer(magpieServer, gobPieConfiguration);
 
         assertTrue(systemOut.getLines().anyMatch(line -> line.contains(" Waiting for command: ")));
         assertTrue(systemOut.getLines().anyMatch(line -> line.contains("Executing ")));
 
-**/
+        **/
     }
 
 
@@ -68,7 +78,6 @@ public class GoblintServerTest {
         GobPieException thrown = assertThrows(GobPieException.class, goblintServer::checkGoblintVersion);
         assertEquals("Checking version failed.", thrown.getMessage());
     }
-
 
 
 }
